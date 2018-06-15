bbe121052a1c8bb4a7f9ebf208050fb6242dcd47
Yejin Kim <kimyj6631@gmail.com>
2018-06-15 10:18:29 +0900

    aopcall 프로젝트 사전작업 시간

--- src/main/java/kr/hs/emirim/kimyj6631/aopcal/RecCalculator.java
+++ src/main/java/kr/hs/emirim/kimyj6631/aopcal/RecCalculator.java
@@ -4,9 +4,16 @@ public class RecCalculator implements Calculator {
 
 	@Override
 	public long factorial(long num) {
+		long start = System.currentTimeMillis();
+		
+		try {
 		if (num == 0)
 			return 1;
 		else
 			return num * factorial(num - 1);
+		}finally {
+			long end = System.currentTimeMillis();
+			System.out.printf("Rec factor[a][%d] : 실행시간= %d",num,(end-start));
+		}
 	}
 }
